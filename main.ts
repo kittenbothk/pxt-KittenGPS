c7ff36e91ebf44ee5a38ca14effdce5369229624
kittenbothk <kittenbotdummy@gmail.com>
2022-05-11 12:22:55 +0800

    time test

--- main.ts
+++ main.ts
@@ -2,6 +2,10 @@
 
 namespace KittenGPS{
 let SerialData='xxxxxxxx'
+let serialBuf=[]
+let UTC=""
+let longitude=""
+let latitude=""
 /**
    * init serial port
    * @param tx Tx pin; eg: SerialPin.P1
@@ -20,9 +24,17 @@ let SerialData='xxxxxxxx'
   SerialData='xxxxxxxx'
   //SerialData=serial.readString()
   while (!SerialData.includes("GNGGA")){
-    SerialData=serial.readString()
+    SerialData=serial.readLine()
+    SerialData="$GNGGA,130651.000,2234.88821,N,11352.29253,E,1,21,0.9,17.8,M,-3.8,M,,*67"
+    serialBuf=SerialData.split(",")
     basic.pause(100)
   }
+  if (serialBuf.length>4){
+  UTC=serialBuf[1]
+  latitude=serialBuf[2]
+  longitude=serialBuf[4]
+  }
+
   }
 
   //% blockId=gps_get block="GPS Get Data"
@@ -30,6 +42,18 @@ let SerialData='xxxxxxxx'
   export function gps_get(): string{
     return SerialData
   }
+
+  //% blockId=gps_utch block="GPS get UTC Time %timeindex"
+  //% group="Basic" weight=85
+  export function gps_utch(i:timeindex): number{
+  let time=[]
+  time[0]=parseFloat(UTC.substr(0,2))
+  time[0]=time[0]+8
+  time[1]=parseFloat(UTC.substr(2,2))
+  time[2]=parseFloat(UTC.substr(4,2))
+  return time[i]
+  }
+
 
 }
 
